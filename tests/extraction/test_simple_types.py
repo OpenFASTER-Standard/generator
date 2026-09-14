4b625d1b126ce210f9e892baaaa1b3ce88762aef
Julian Nalenz <julian.nalenz@divizend.com>
2026-09-14 15:00:05 +0000

    feat: add facet and xs:union extraction for simple types

---
+++ tests/extraction/test_simple_types.py
@@ -0,0 +1,76 @@
+"""Tests for facet and xs:union extraction, against real MiKaDiv-FM
+simple types -- the complete closed set of 12 standard facets, not
+just the subset this project's own census happened to observe."""
+from rdflib import RDF, Graph, Namespace, URIRef
+import xmlschema
+
+from extraction.simple_types import XSDO, extract_simple_type
+
+FIXTURE = "ontologies/mikadiv-fm/sources/xsd/MiKaDiv_FM_Standardtypen_1.02.xsd"
+EX = Namespace("https://example.org/test/")
+
+
+def _schema():
+    return xmlschema.XMLSchema(FIXTURE)
+
+
+def test_enumeration_facet_is_extracted_as_a_literal_value_per_option():
+    schema = _schema()
+    xsd_type = schema.maps.types["{http://www.itzbund.de/MiKaDiv/FMStd/1.02}NatuerlicheGeschlechtType"] \
+        if "{http://www.itzbund.de/MiKaDiv/FMStd/1.02}NatuerlicheGeschlechtType" in schema.maps.types \
+        else next(
+            t for name, t in schema.maps.types.items()
+            if "{http://www.w3.org/2001/XMLSchema}enumeration" in getattr(t, "facets", {})
+        )
+    uri = EX.SomeEnumType
+    graph = Graph()
+
+    extract_simple_type(graph, xsd_type, uri)
+
+    values = {
+        str(graph.value(v, XSDO.literalValue))
+        for v in graph.objects(uri, XSDO.hasEnumerationValue)
+    }
+    real_enum = xsd_type.facets["{http://www.w3.org/2001/XMLSchema}enumeration"].enumeration
+    assert values == set(real_enum)
+
+
+def test_length_and_pattern_facets_are_extracted_from_wid_type():
+    schema = _schema()
+    xsd_type = schema.maps.types["{http://www.itzbund.de/MiKaDiv/FMStd/1.02}WIDType"]
+    uri = EX.WIDType
+    graph = Graph()
+
+    extract_simple_type(graph, xsd_type, uri)
+
+    assert (uri, RDF.type, XSDO.SimpleTypeDefinition) in graph
+    assert int(graph.value(uri, XSDO.length)) == 16
+    patterns = {str(p) for p in graph.objects(uri, XSDO.pattern)}
+    assert patterns == {"DE[0-9]{14}"}
+
+
+def test_union_member_types_are_linked_by_their_own_real_uri():
+    schema = _schema()
+    xsd_type = schema.maps.types["{http://www.itzbund.de/MiKaDiv/FMStd/1.02}GeburtsdatumType"]
+    uri = EX.GeburtsdatumType
+    graph = Graph()
+
+    extract_simple_type(graph, xsd_type, uri)
+
+    members = {str(m) for m in graph.objects(uri, XSDO.hasUnionMember)}
+    assert members == {
+        "http://www.itzbund.de/MiKaDiv/FMStd/1.02#Datum0Type",
+        "http://www.itzbund.de/MiKaDiv/FMStd/1.02#Datum1880Type",
+        "http://www.itzbund.de/MiKaDiv/FMStd/1.02#Datum0000Type",
+    }
+
+
+def test_type_with_no_facets_still_gets_the_definition_triple():
+    schema = _schema()
+    xsd_type = schema.maps.types["{http://www.itzbund.de/MiKaDiv/FMStd/1.02}Datum0Type"]
+    uri = EX.Datum0Type
+    graph = Graph()
+
+    extract_simple_type(graph, xsd_type, uri)
+
+    assert (uri, RDF.type, XSDO.SimpleTypeDefinition) in graph
